7be9d41bce46ca537dd9f42c4b98f4c35d0f07a9
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2023-12-07 15:53:15 +0100

    final solution

---
+++ problems/1903.largest.odd.number.in.string/largestOddNumber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+      count = len(num)-1
+      for char in reversed(num):
+        if int(char) % 2 != 0:  
+          return num[:count+1]
+        count -= 1
+      return ''          
+        
+num = "52"   
+sol1 = Solution()
+result = sol1.largestOddNumber(num)
+print(result)        
