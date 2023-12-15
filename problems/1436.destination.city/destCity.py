48907003a40e9891fe89167c01491e55352d8807
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2023-12-15 15:41:58 +0100

    add solution

---
+++ problems/1436.destination.city/destCity.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        cities = set()
+
+        for path in paths:
+            cities.add(path[0])
+
+        for path in paths:
+            dest = path[1]
+            if dest not in cities:
+                return dest 
+        return ""
+paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+sol1 = Solution()
+result = sol1.destCity(paths)
+print(result)
+        
