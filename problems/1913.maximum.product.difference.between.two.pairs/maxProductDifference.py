58b46c79a16dd64dac99373d4efa991351b6b8b5
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2023-12-18 14:39:43 +0100

    refactoring

--- problems/1913.maximum.product.difference.between.two.pairs/maxProductDifference.py
+++ problems/1913.maximum.product.difference.between.two.pairs/maxProductDifference.py
@@ -2,8 +2,7 @@ from typing import List
 
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
-        sorted = nums.sort()
-
+        nums.sort()
         return (nums[len(nums)-1]*nums[len(nums)-2])-(nums[0]*nums[1])
 
 nums = [5,6,2,7,4]
