b521558b0ab2dcaa806352dd294ddde3f6eebaf4
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-01-15 15:22:48 +0100

    delete personal comments

--- problems/2225.find.players.with.zero.or.one.losses/findWinners.py
+++ problems/2225.find.players.with.zero.or.one.losses/findWinners.py
@@ -1,14 +1,6 @@
 from pstats import SortKey
 from typing import List  
 
-# voglio in pos 0 tutti quelli che non hanno perso neanche un match
-# voglio in pos 1 tutti che hanno perso esattamente un match  
-
-# pos 0 : associo ad ogni elemento che incontro per la prima volta un contatore, e metto l'elemento nella lista di pos 1. nel momento in cui re-incontro il contatore, quindi il contatore associato mi va a 1, elimino l'elemento dalla lista ( tengo il contatore per i controlli successivi)
-# pos 1:  stessa concetto: associo ad ogni elemento un contatore a zero, che incremento nel momento in cui li incontro che hanno perso. se il contatore è > di zero elimino gli elementi dalla lista 
-
-# ho quindi bisogno di di una struttura dati ausiliaria: combino le due condizioni di pos0 e pos1, e utilizzo una mappa da elemento a intero che mi conta quanti match ha perso.
-# eseguo una prima scansione per aggiornare i contatori, successivamente itero sulla mappa e, dove la condizione viene rispettata, aggiungo l'elemento nella lista di pos0 e/o nella lista di pos 1
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         counter = {}
