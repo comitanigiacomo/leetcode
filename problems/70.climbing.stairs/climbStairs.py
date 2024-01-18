f7862369a7016bb4a83f0675c6a4385241bd48f4
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-01-18 12:24:38 +0100

    add solution

---
+++ problems/70.climbing.stairs/climbStairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b, c = 0, 1, 0
+        
+        for i in range(n):
+            c = a + b
+            a, b = b, c
+        
+        return b
+        
+n = 2
+sol1 = Solution()
+print(sol1.climbStairs(n))
