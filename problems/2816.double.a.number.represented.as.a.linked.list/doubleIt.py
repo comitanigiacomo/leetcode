2901b2744d25a90ff9352beecb10773f0b23a796
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-05-07 12:59:06 +0200

    final solution

--- problems/2816.double.a.number.represented.as.a.linked.list/doubleIt.py
+++ problems/2816.double.a.number.represented.as.a.linked.list/doubleIt.py
@@ -1,32 +1,22 @@
+from typing import Optional
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-from typing import Optional
-
-
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        string_num = ''
-        current = head
-        while current: 
-            string_num = string_num + str(current.val)
-            current = current.next
-        
-        num = int(string_num) * 2
-        lst = [int(i) for i in str(num)]
-
         current = head 
-        i = 0
-        while i < len(lst):
-            current.val = lst[i]
-            if current.next is None:
-                if i < len(lst) - 1:
-                    current.next = ListNode(0)
-                else:
-                    break
-            current = current.next
-            i += 1
-        
+        if head.val >= 5 :
+            head = ListNode(1, head)    
+        while current :
+            current.val = (current.val * 2) % 10
+            if current.next:
+                successivo = current.next 
+                if (successivo.val * 2 ) >= 10: current.val = current.val + 1 
+            current = current.next 
         return head
+        
+            
+        
