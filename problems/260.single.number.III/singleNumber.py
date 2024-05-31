fcb800e1b10ff0ee37232447b11e4a3945a1bac5
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-05-31 09:48:10 +0200

    refactor using set

--- problems/260.single.number.III/singleNumber.py
+++ problems/260.single.number.III/singleNumber.py
@@ -2,13 +2,13 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        out = []
+        out = set()
         for i in range(len(nums)):
             if nums[i]  in out :
                 out.remove(nums[i])
             else:
-                out.append(nums[i])
-        return out
+                out.add(nums[i])
+        return list(out)
         
 nums = [-1, 0]
 sol1 = Solution()
