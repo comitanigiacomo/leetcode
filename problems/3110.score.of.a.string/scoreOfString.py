77502b37a4017a6e20885dc18b76f58256427b7e
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-06-01 12:30:48 +0200

    add solution of the problem

---
+++ problems/3110.score.of.a.string/scoreOfString.py
@@ -0,0 +1,10 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        sum = 0
+        for i in range(0, len(s)-1):
+            sum += abs(ord(s[i])- ord(s[i+1]))
+        return sum
+        
+s = "hello"
+sol1 = Solution()
+print(sol1.scoreOfString(s))
