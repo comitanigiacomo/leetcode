91d931e0b4d0ea0e2bffa159ced231ab3948903c
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-06-02 12:58:02 +0200

    add solution of the problem

---
+++ problems/344.reverse.string/reverseString.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+        
+        
+s = ["h","e","l","l","o"]
+sol1 = Solution()
+sol1.reverseString(s)
