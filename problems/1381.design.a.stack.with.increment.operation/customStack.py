566b78a36be299bd7b67ff1f73ce99c3bc42a08d
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-09-30 10:47:54 +0200

    add solution of the prroblem

---
+++ problems/1381.design.a.stack.with.increment.operation/customStack.py
@@ -0,0 +1,19 @@
+class CustomStack:
+    
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.maxSize = maxSize
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.maxSize:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if len(self.stack) == 0:
+            return -1
+        return self.stack.pop()
+
+    def increment(self, k: int, val: int) -> None:
+        for i in range(len(self.stack)):
+            if i < k: 
+                self.stack[i] += val
