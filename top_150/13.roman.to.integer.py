f63a86842b1f499c20be550adfc9243893aabe8d
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2024-10-01 22:43:44 +0200

    add solution of the problem

---
+++ top_150/13.roman.to.integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        conversions = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        out = 0
+        for i in range(len(s)):
+            current = conversions[s[i]]
+            if i < len(s)-1 and current < conversions[s[i+1]]:
+                out -= current
+            else: 
+                out += current
+        return out
+        
+        
+s = "LVIII"
+sol = Solution()
+print(sol.romanToInt(s))
