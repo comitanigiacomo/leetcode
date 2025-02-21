2a84ccd0041bf5ca26a8d6c4c1adcb78d49c77d3
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-02-21 10:29:02 +0100

    add solution of the problem

---
+++ problems/1261.find.elements.in.a.contaminated.binary.tree/Solution.py
@@ -0,0 +1,15 @@
+from collections import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        
+        
+
+    def find(self, target: int) -> bool:
+        return False
