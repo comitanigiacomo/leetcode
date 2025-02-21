9e5561d5c092e29ab6071cc2abe5021c282a2a13
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-02-21 11:32:39 +0100

    add solution of the problem

--- problems/1261.find.elements.in.a.contaminated.binary.tree/Solution.py
+++ problems/1261.find.elements.in.a.contaminated.binary.tree/Solution.py
@@ -1,4 +1,4 @@
-from collections import Optional
+from collections import Optional, deque
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -9,7 +9,17 @@ class FindElements:
 
     def __init__(self, root: Optional[TreeNode]):
         
-        
-
+        self.values = set()
+        if root:
+            self.recover_tree(root, 0)
+            
+    def recover_tree(self, node: Optional[TreeNode], value: int):
+        if not node:
+            return
+        node.val = value
+        self.values.add(value)
+        self.recover_tree(node.left, 2 * node.val + 1)
+        self.recover_tree(node.right, 2 * node.val + 2)
+                
     def find(self, target: int) -> bool:
-        return False
+        return target in self.values
