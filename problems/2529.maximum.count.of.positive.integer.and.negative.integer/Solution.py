11383bd1dbecb80c06869605119a98ac941e0239
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-03-12 16:30:17 +0100

    refactor: using binary search for finding zeroes in the array

--- problems/2529.maximum.count.of.positive.integer.and.negative.integer/Solution.py
+++ problems/2529.maximum.count.of.positive.integer.and.negative.integer/Solution.py
@@ -1,17 +1,41 @@
 from typing import List
 
 class Solution:
-    def maximumCount(self, nums: List[int]) -> int:
-        neg = 0
-        pos = len(nums)
+    def negative(self, nums):
+        start, end = 0, len(nums)-1
+        index = len(nums)
 
-        for i in range(len(nums)):
-            elem = nums[i]
-            if elem < 0: neg += 1
-            if elem > 0: 
-                pos = i
-                break
-        return max(neg, len(nums)-pos)
+        while start <= end:
+            mid = (start + end)// 2
+
+            if nums[mid] < 0:
+                start = mid + 1
+            else:
+                end = mid - 1
+                index = mid
+                
+        return index
+
+    def positive(self, nums):
+        start, end = 0, len(nums)-1
+        index = len(nums)
+
+        while start <= end:
+            mid = (start + end) // 2
+
+            if nums[mid] <= 0:
+                start = mid + 1
+            else:
+                end = mid -1
+                index = mid
+                
+        return index
+
+    def maximumCount(self, nums: List[int]) -> int:
+        positive_count = len(nums) - self.positive(nums)
+        negative_count = self.negative(nums)
+
+        return max(positive_count, negative_count)
         
 nums = [-2,-1,-1,1,2,3]
 print(Solution().maximumCount(nums))   
