d85c86f1d57b933cef80ca42ee87db8a8ca714ad
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-05-20 23:42:45 +0200

    Add solution of the problem

--- problems/3355.zero.array.transformation.i/Solution.py
+++ problems/3355.zero.array.transformation.i/Solution.py
@@ -3,7 +3,28 @@ from typing import List
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
 
-      return True
+        n  = len(nums)
+
+        df = [0] * (n+1)
+
+        for l, r in queries:
+            df[l] += 1
+
+            if r+1 < len(df):
+                df[r+1] -= 1
+
+        pf = [0]*n
+        summ = 0
+
+        for i in range(n):
+            summ += df[i]
+            pf[i] = summ
+
+        for i in range(n):
+            if nums[i] > pf[i]:
+                return False
+
+        return True
 
 nums = [1, 0, 1]
 queries = [[0,2]]
