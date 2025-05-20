858f1ab5be350ef5ff56b5cd5f98ffefa479c792
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-05-20 23:40:11 +0200

    Add solution of the problem

---
+++ problems/3355.zero.array.transformation.i/Solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+
+      return True
+
+nums = [1, 0, 1]
+queries = [[0,2]]
+sol = Solution()
+print(sol.isZeroArray(nums, queries))
