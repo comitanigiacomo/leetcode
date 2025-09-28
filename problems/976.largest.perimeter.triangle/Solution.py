095f002bb1b00ef692a29bad5c87d6bb8e3df3f7
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-09-28 16:06:38 +0200

    add solution of the problem

---
+++ problems/976.largest.perimeter.triangle/Solution.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+from typing import List
+
+class Solution:
+    
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        
+        for i in range(len(nums)-2):
+                        
+            if nums[i] < nums[i+1] + nums[i+2]:
+                return nums[i] + nums[i+1] + nums[i+2]
+            
+        return 0
+            
+                
+nums = [1,2,1,10]
+sol = Solution()
+print(sol.largestPerimeter(nums))
