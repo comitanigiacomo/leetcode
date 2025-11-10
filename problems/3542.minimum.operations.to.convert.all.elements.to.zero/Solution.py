a0ccd4d361c9d2e1fed51d00afe2270019d4b797
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-11-10 11:42:01 +0100

    add solution of the problem

---
+++ problems/3542.minimum.operations.to.convert.all.elements.to.zero/Solution.py
@@ -0,0 +1,20 @@
+from typing import List
+import heapq
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        count = 0
+        stack = []
+
+        for elem in nums:
+            while stack and stack[-1] > elem:
+                stack.pop()
+            if elem > 0 and (len(stack) == 0 or stack[-1] < elem):
+                stack.append(elem)
+                count += 1
+
+        return count
+
+nums = [3,1,2,1]
+sol = Solution()
+print(sol.minOperations(nums))
